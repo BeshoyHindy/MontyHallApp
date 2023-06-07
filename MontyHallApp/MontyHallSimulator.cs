83320037a1f5426b02a70b1f7068e43b9275b79d
Beshoy Hindy <beshoy.hindy@gmail.com>
2023-06-07 02:55:13 +0200

    Add Monty Hall Console App.

---
+++ MontyHallApp/MontyHallSimulator.cs
@@ -0,0 +1,35 @@
+﻿namespace MontyHallApp;
+
+class MontyHallSimulator
+{
+    private const int DoorCount = 3;
+    private readonly Random _random = new Random();
+
+    public bool Simulate(bool changeDoor)
+    {
+        var carDoor = _random.Next(DoorCount);
+        var playerChoice = _random.Next(DoorCount);
+        int openDoor;
+        do
+        {
+            openDoor = _random.Next(DoorCount);
+        } while (openDoor == carDoor || openDoor == playerChoice);
+
+        if (changeDoor)
+        {
+            playerChoice = DoorCount - playerChoice - openDoor;
+        }
+        return playerChoice == carDoor;
+    }
+
+    public void RunSimulations(int numberOfSimulations, bool changeDoor)
+    {
+        var results = Enumerable.Range(0, numberOfSimulations).Select(_ => Simulate(changeDoor));
+        var wins = results.Count(win => win);
+        var winsPercent = (double)wins / numberOfSimulations * 100;
+
+        Console.WriteLine($"Out of {numberOfSimulations}, there were {wins} wins.");
+        Console.WriteLine($"Win percentage: {winsPercent}%");
+    }
+}
+
---
+++ MontyHallApp/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MontyHallApp;
+
+while (true)
+{
+    var numberOfSimulations = GetSimulationCount();
+    var changeDoor = GetChangeDoorPreference();
+
+    var simulator = new MontyHallSimulator();
+    simulator.RunSimulations(numberOfSimulations, changeDoor);
+}
+
+static int GetSimulationCount()
+{
+    while (true)
+    {
+        Console.Write("Enter the number of simulations: ");
+        if (int.TryParse(Console.ReadLine(), out var numberOfSimulations) && numberOfSimulations > 0)
+        {
+            return numberOfSimulations;
+        }
+        Console.WriteLine("Please enter a valid number greater than 0.");
+    }
+}
+
+static bool GetChangeDoorPreference()
+{
+    while (true)
+    {
+        Console.Write("Change door? [yes/no]: ");
+        var input = Console.ReadLine()?.ToLower();
+        if (input == "yes") return true;
+        if (input == "no") return false;
+
+        Console.WriteLine("Please answer with 'yes' or 'no'");
+    }
+}
+
